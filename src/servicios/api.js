f21122d4ee4866b39991d8b297373a88aca5fd00
yogesh <yogeshmangule@gmail.com>
2024-08-30 17:34:21 +0530

    update api

--- src/servicios/api.js
+++ src/servicios/api.js
@@ -1,6 +1,6 @@
 // const api = "https://quality.nochemadrid.org/api/";
-// const api = "https://apiquality.fiestaismadrid.net/"
-const api = 'http://localhost:8000/';
+const api = "https://apiquality.fiestaismadrid.net/"
+// const api = 'http://localhost:8000/';
 const varAdmin = "administrators";
 const varEncu = "encuestador";
 const varOpe = "operador";
